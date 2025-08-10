25de48c854fc90aa1c841cc458f9bc5c07e25737
Andrii Ferenchuk <ferenchuk.a@gmail.com>
2025-08-10 23:34:31 +0300

    add redux, add and display books

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,15 +1,22 @@
 import { useState } from "react"
 import "./BookForm.css"
+import { useDispatch } from "react-redux"
+import { addBook } from "../../redux/books/actionCreators"
 
 const BookForm = () => {
   const [title, setTitle] = useState("")
   const [author, setAuthor] = useState("")
+  const dispatch = useDispatch()
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (title && author) {
-      // console.log(title, author)
+      const book = {
+        title,
+        author,
+      }
+      dispatch(addBook(book))
       setTitle("")
       setAuthor("")
     }
